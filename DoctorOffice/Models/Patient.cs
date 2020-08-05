2594b7013caa539e7a41b2cec572548b65ea24ea
Beverly Potts <pottsbeverly@gmail.com>
2020-08-05 17:36:15 -0400

    Add all models
    co-authored-by: Cody Fritz <clanalia55@gmail.com>

---
+++ DoctorOffice/Models/Patient.cs
@@ -0,0 +1,15 @@
+namespace DoctorOffice.Models
+{
+  public class Patient
+  {
+    public Patient()
+    {
+      this.Doctors = new HashSet<DoctorPatient>();
+    }
+
+    public int PatientId {get; set;}
+    public string Name {get; set;}
+
+    public ICollection<DoctorPatient> Doctors{get;}
+  }
+}
